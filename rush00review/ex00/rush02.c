cb316c646608137f40f64e127dccd3053b350f27
Fgreco01 <118677474+Fgreco01@users.noreply.github.com>
2022-11-21 14:15:22 +0100

    Add files via upload

---
+++ rush00review/ex00/rush02.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prova00.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgreco <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/20 13:52:30 by fgreco            #+#    #+#             */
+/*   Updated: 2022/11/21 11:11:11 by fgreco           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_print_top_lane(int x)
+{
+	int	i;
+
+	i = 1;
+	while (i <= x)
+	{
+		if (i == 1 || i == x)
+		{
+			ft_putchar ('A');
+		}
+		if (i > 1 && i < x)
+		{
+			ft_putchar ('B');
+		}
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+void	ft_print_mid_lanes(int x, int y)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 2;
+	while (i <= x && j < y)
+	{
+		if (i == 1 || i == x)
+		{
+			ft_putchar('B');
+		}
+		if (i > 1 && i < x)
+		{
+			ft_putchar(' ');
+		}
+		i++;
+		if (i == x + 1)
+		{
+			ft_putchar('\n');
+			j++;
+			i = 1;
+		}
+	}
+}
+
+void	ft_print_bot_lane(int x, int y)
+{
+	int	i;
+
+	i = 1;
+	if (y > 1)
+	{
+		while (i <= x)
+		{
+			if (i == 1 || i == x)
+			{
+				ft_putchar('C');
+			}
+			if (i > 1 && i < x)
+			{
+				ft_putchar('B');
+			}
+			i++;
+		}
+		ft_putchar('\n');
+	}
+}
+
+void	rush(int x, int y)
+
+{
+	if (x == 0 || y <= 0)
+		return ;
+	ft_print_top_lane(x);
+	ft_print_mid_lanes(x, y);
+	ft_print_bot_lane(x, y);
+}
